a964ce8c9b56a4de54bde1cb3dcc557ecdb6b172
MAS0002 <mas.info0220@gmail.com>
2025-04-08 16:26:47 +0500

    Add files via upload

---
+++ Installing Libraries.py
@@ -0,0 +1,19 @@
+print("hello, Python World")
+# today i am going to install basic libraries
+# Few minutes later
+
+
+#Finally installed libraries lets write some programs
+
+#Program to Print the table 
+
+# Taking input from the user
+number = int(input("Enter the number to print its table: "))
+
+# Loop to generate the table
+for i in range(1, 11):  # This loop runs from 1 to 10
+    print(f"{number} x {i} = {number * i}")
+
+
+
+
